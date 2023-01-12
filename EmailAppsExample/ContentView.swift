dc4f4bd0ae2d3e4269cf6f201c94bdb2c8821449
Alex Kostenko <kostenkoaleksander7@gmail.com>
2023-01-12 14:41:27 +0200

    Implement email app picker example.

--- EmailAppsExample/ContentView.swift
+++ EmailAppsExample/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import EmailApps
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Send") {
+            showEmailPicker = true
+        }
+        .actionSheet(isPresented: $showEmailPicker) {
+            .init(title: Text("Send with ..."), buttons: actionButtons())
         }
         .padding()
+    }
+
+    @State private var showEmailPicker = false
+    @State private var emailApps = [any EmailApp]()
+}
+
+extension ContentView {
+    private func actionButtons() -> [ActionSheet.Button] {
+        EmailApps.supported.compactMap { app in
+            guard let url = app.url(email: "example@gmail.com",
+                                    subject: "Hello world!",
+                                    body: "Here is my letter to you.") else {
+                return nil
+            }
+
+            return  .default(Text(app.name)) {
+                UIApplication.shared.open(url)
+            }
+        } + [.cancel(Text("Cancel"))]
     }
 }
 
