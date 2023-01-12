d002237dc46a1f16df193091b7ad1bb5de842604
Alex Kostenko <kostenkoaleksander7@gmail.com>
2023-01-12 14:06:59 +0200

    Initial Commit

---
+++ EmailAppsExample/EmailAppsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailAppsExampleApp.swift
+//  EmailAppsExample
+//
+//  Created by Alex Kostenko on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmailAppsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
